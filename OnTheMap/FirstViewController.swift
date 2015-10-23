f1dd63fb7da8b6d023f4896d22dd12e37f149efd
Max Giraldo <maxagiraldo@gmail.com>
2015-10-23 11:24:01 -0700

    Initial Commit

---
+++ OnTheMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OnTheMap
+//
+//  Created by Maximilian A. Giraldo on 10/23/15.
+//  Copyright © 2015 Maximilian A. Giraldo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by Maximilian A. Giraldo on 10/23/15.
+//  Copyright © 2015 Maximilian A. Giraldo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
